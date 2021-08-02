481bae0b33b0f531d71b8dd519f24174e82039d3
Sthefany Gomes <fany@ubuntu.local>
2021-08-02 20:51:26 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let tower1 = document.createElement("div");
+    tower1.id = "tower1";
+    tower1.className = "towers";
+
+let tower2 = document.createElement("div");
+    tower2.id = "tower2";
+    tower2.className = "towers";
+
+let tower3 = document.createElement("div");
+    tower3.id = "tower3";
+    tower3.className = "towers";
+
+let container = document.getElementById("container");
+    container.appendChild(tower1);
+    container.appendChild(tower2);
+    container.appendChild(tower3);
+
+
+let disc1 = document.createElement("div");
+    disc1.id = "disc1";
+    disc1.className = "discs"
+
+let disc2 = document.createElement("div");
+    disc2.id = "disc2";
+    disc2.className = "discs"
+
+let disc3 = document.createElement("div");
+    disc3.id = "disc3";
+    disc3.className = "discs"
+
+let disc4 = document.createElement("div");
+    disc4.id = "disc4";
+    disc4.className = "discs"
+
+    tower1.appendChild(disc1);
+    tower1.appendChild(disc2);
+    tower1.appendChild(disc3);
+    tower1.appendChild(disc4);
+
+    
